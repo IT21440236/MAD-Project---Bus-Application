dd85f71fc20d2655631e29856a4233657a84c321
Yeran Kodithuwakku <kodithuwakkuyeran@gmail.com>
2023-01-19 19:11:12 +0530

    Bus Application - commit 01

---
+++ app/src/main/java/com/model/busapplicatiion/MainActivity.java
@@ -0,0 +1,14 @@
+package com.model.busapplicatiion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
